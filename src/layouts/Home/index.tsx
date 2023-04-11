6228a8bb89ad023b9cbd56885e1944afd2c829f4
juniorvilas <log_junior@hotmail.com>
2023-04-11 17:14:58 -0300

    feat: create home page to list users

--- src/layouts/Home/index.tsx
+++ src/layouts/Home/index.tsx
@@ -1,3 +1,46 @@
+import { useGetUsers } from 'hooks/useGetUsers';
+import * as S from './styles';
+import { CardUser } from 'components/CardUser';
+import { Button } from '@mui/material';
+
 export function HomePage() {
-  return <div>oi</div>;
+  const { data, incrementPage, decrementPage, isLoading, page } = useGetUsers({
+    results: 9
+  });
+
+  console.log(data);
+
+  if (isLoading) return <p>Carregando...</p>;
+  return (
+    <>
+      <S.Title>Usuários</S.Title>
+      <S.Container>
+        {data?.results.map((user, index) => (
+          <div key={index}>
+            <CardUser
+              name={user.name.first}
+              img={user.picture.large}
+              lastName={user.name.last}
+              email={user.email}
+              birthDate={user.dob.date}
+              address={user.location.street.name}
+              phone={user.phone}
+            />
+          </div>
+        ))}
+      </S.Container>
+      <S.ContainerButton>
+        <Button
+          disabled={page === 1}
+          variant="contained"
+          onClick={decrementPage}
+        >
+          Anterior
+        </Button>
+        <Button variant="contained" onClick={incrementPage}>
+          Proxima
+        </Button>
+      </S.ContainerButton>
+    </>
+  );
 }
---
+++ src/layouts/Home/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin: 0 auto;
+`;
+
+export const ContainerButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  margin-top: 25px;
+`;
+
+export const Title = styled.h2`
+  font-size: 35px;
+  font-weight: 500;
+  text-align: center;
+  margin: 25px 0;
+`;
